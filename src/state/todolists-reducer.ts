df0e61d856f7a0600ee38bf6bf2857f414c7e98a
makcsholonik <makcsholonik@gmail.com>
2021-11-28 20:51:25 +0300

    08 (ActionType)

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,9 +1,28 @@
-import { TodolistsType } from '../App';
+import { TaskFilterType, TodolistsType } from '../App';
 import { v1 } from 'uuid';
 
-type ActionType = {
-	type : string
-	[ key : string ] : any
+type ActionType = RemoveTodolistActionType
+	| AddTodolistActionType
+	| ChangeTodolistTitleActionType
+	| ChangeTodolistFilterActionType
+
+export type RemoveTodolistActionType = {
+	type : 'REMOVE-TODOLIST'
+	id : string
+}
+export type AddTodolistActionType = {
+	type : 'ADD-TODOLIST'
+	title : string
+}
+export type ChangeTodolistTitleActionType = {
+	type : 'CHANGE-TODOLIST-TITLE'
+	id : string
+	title : string
+}
+export type ChangeTodolistFilterActionType = {
+	type : 'CHANGE-TODOLIST-FILTER'
+	id : string
+	filter : TaskFilterType
 }
 
 // меня вызовут и дадут мне стейт (почти всегда объект)
