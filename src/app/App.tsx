7454df5973d67084e3c77b2c83a36cd3c5a0db43
makcsholonik <makcsholonik@gmail.com>
2022-02-08 23:18:16 +0300

    16 (Route, Login)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,36 +7,42 @@ import { ErrorSnackbar } from '../component/ErrorSnackbar/ErrorSnackbar';
 import { useSelector } from 'react-redux';
 import { AppRootStateType } from '../state/store';
 import { StatusType } from '../state/app-reducer';
+import { Login } from '../features/Login/Login';
+import { Routes, Route } from 'react-router-dom';
 
 type AppPropsType = {
 	demo? : boolean
 }
 
-export const App = React.memo ( ({demo = false}: AppPropsType) => {
+export const App = React.memo ( ( { demo = false } : AppPropsType ) => {
 
-	const status = useSelector<AppRootStateType, StatusType> ( state => state.app.status );
+		const status = useSelector<AppRootStateType, StatusType> ( state => state.app.status );
 
-	return (
-		<div className="App">
-			<Box>
-				<AppBar position={ 'static' } color={ 'primary' }>
-					<Toolbar>
-						<IconButton edge={ 'start' } color={ 'inherit' } aria-label={ 'menu' }>
-							<Menu/>
-						</IconButton>
-						<Typography variant={ 'h6' }>
-							News
-						</Typography>
-						<Button color={ 'inherit' }>Login</Button>
-					</Toolbar>
-					{ status === 'loading' && <LinearProgress color="secondary"/> }
-				</AppBar>
-			</Box>
-			<ErrorSnackbar/>
-			<Container fixed>
-				<TodolistsLists demo={demo}/>
-			</Container>
-		</div>
-	);
-} );
+		return (
+			<div className="App">
+				<Box>
+					<AppBar position={ 'static' } color={ 'primary' }>
+						<Toolbar>
+							<IconButton edge={ 'start' } color={ 'inherit' } aria-label={ 'menu' }>
+								<Menu/>
+							</IconButton>
+							<Typography variant={ 'h6' }>
+								News
+							</Typography>
+							<Button color={ 'inherit' }>Login</Button>
+						</Toolbar>
+						{ status === 'loading' && <LinearProgress color="secondary"/> }
+					</AppBar>
+				</Box>
+				<ErrorSnackbar/>
+				<Container fixed>
+					<Routes>
+						<Route path={ '/' } element={ <TodolistsLists demo={ demo }/> }/>
+						<Route path={ '/login' } element={ <Login/> }/>
+					</Routes>
+				</Container>
+			</div>
+		);
+	}
+);
 
